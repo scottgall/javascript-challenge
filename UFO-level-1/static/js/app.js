315b1db2ac47eb875c44932928cc784a090df3de
Scott Gall <scottarthurgall@gmail.com>
2021-05-15 15:02:22 -0500

    added date filter functionality

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+let form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let date = document.querySelector('#datetime').value;
+  let filteredData = tableData.filter(obj => obj.datetime === date);
+  let rows = createRows(filteredData);
+  updateDOM(rows);
+});
+
+let createRows = (data) => {
+  html = '';
+  data.forEach(obj => {
+    html += `<tr>
+              <td>${obj.datetime}</td>
+              <td>${obj.city}</td>
+              <td>${obj.state}</td>
+              <td>${obj.country}</td>
+              <td>${obj.shape}</td>
+              <td>${obj.duration}</td>
+              <td>${obj.comments}</td>
+            </tr>`
+  });
+  return html;
+};
+
+let updateDOM = (html) => {
+  document.querySelector('tbody').innerHTML = html;
+};
+
+updateDOM(createRows(tableData));
+
