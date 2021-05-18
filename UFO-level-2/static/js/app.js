2b16182cac729f5ada0da9a0c3e688f7a447743f
Scott Gall <scottarthurgall@gmail.com>
2021-05-17 21:13:12 -0500

    added js to part 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+let form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let date = document.querySelector('#datetime').value;
+  if (date) {
+    let filteredData = tableData.filter(obj => obj.datetime === date);
+    let rows = createRows(filteredData);
+    updateDOM(rows);
+  };
+});
+
+let createRows = (data) => {
+  return data.reduce((prev, cur) => {
+    return prev + `<tr>
+                    <td>${cur.datetime}</td>
+                    <td>${cur.city}</td>
+                    <td>${cur.state}</td>
+                    <td>${cur.country}</td>
+                    <td>${cur.shape}</td>
+                    <td>${cur.durationMinutes}</td>
+                    <td>${cur.comments}</td>
+                  </tr>`
+  }, '')
+};
+
+let updateDOM = (html) => {
+  document.querySelector('tbody').innerHTML = html;
+};
+
+updateDOM(createRows(tableData));
